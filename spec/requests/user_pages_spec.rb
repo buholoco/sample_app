e37f01e9051ab9230a7c2b2c2a6f1972d2b7e1d7
buholoco <buholoco@gmail.com>
2013-01-29 17:21:42 -0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,7 @@ describe "User Pages" do
         before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
-        it { should have_content('error') }
+        it { should have_error_message('error') }
       end
     end
 
@@ -43,6 +43,15 @@ describe "User Pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
+      describe "followed by signout" do
+        before do
+          click_button submit
+          click_link "Sign out" 
+        end
+        it { should have_link('Sign in') }
       end
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,11 @@
+require 'rspec/expectations'
 include ApplicationHelper
 
+  RSpec::Matchers.define :have_error_message do |message|
+    match do |page|
+      page.should have_selector('div.alert.alert-error', text: message)
+    end
+  end
 # def full_title(page_title)
 #   base_title = "Ruby on Rails Tutorial Sample App"
 #   if page_title.empty?
