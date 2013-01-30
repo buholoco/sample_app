1f178b7ac07f05465390993fcb614fda901716e7
buholoco <buholoco@gmail.com>
2013-01-30 03:09:41 -0300

    After the git conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,6 @@
 #
 
 class User < ActiveRecord::Base
-FactoryGirl.define do
-  factory :user do
-    sequence(:name)     { |n| "Person #{n}" }
-    sequence(:email)    { |n| "person_#{n}@example.com" }
-    password "foobar"
-    password_confirmation "foobar"
-  end
-end
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
